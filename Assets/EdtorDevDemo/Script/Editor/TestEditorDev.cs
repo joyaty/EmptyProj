520a78044a93d26ba9857c6d5fd8de2f2e25f3a3
DexuanYang <joyat.yang@igg.com>
2022-05-13 20:15:04 +0800

    * 编辑器开发demo

---
+++ Assets/EdtorDevDemo/Script/Editor/TestEditorDev.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Nt.Editor
+{
+    public class PreviewScene : SceneView
+    { 
+    }
+
+
+    public class CustomPreviewSceneStage : PreviewSceneStage
+    {
+        protected override GUIContent CreateHeaderContent()
+        {
+            return new GUIContent("Test");
+        }
+
+        protected override void OnCloseStage()
+        {
+            Debug.Log("close Preview Stage");
+            base.OnCloseStage();
+        }
+
+        protected override bool OnOpenStage()
+        {
+            Debug.Log("OpenStage Preview Stage");
+            return base.OnOpenStage();
+        }
+
+
+    }
+}
+
